0fb34edaa392a39090f1178343e8abdb5bed0e8f
Anurag Pal <anuragpal7754@gmail.com>
2025-01-11 13:25:37 +0530

    "stack and Heap"

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,36 @@
-let score = 990;
+// let score = 990;
 
-let scoreInString = String(score);
-console.log(scoreInString);
-console.log(typeof scoreInString);
+// let scoreInString = String(score);
+// console.log(scoreInString);
+// console.log(typeof scoreInString);
 
-let scoreInBoolean = Boolean(score);
-console.log(scoreInBoolean);
-console.log(typeof scoreInBoolean);
+// let scoreInBoolean = Boolean(score);
+// console.log(scoreInBoolean);
+// console.log(typeof scoreInBoolean);
 
+// let id = Symbol('123');
+// let anotherId = Symbol('123');
+// console.log(id === anotherId);
+
+// stack and heap memory 
+
+
+ // stack(primitive) and Heap(non-primitive);
+ let myYoutubeName = "Anurag Pal";
+
+ let anotherName = myYoutubeName;
+ anotherName = "Anuragpal 2"
+ console.log(anotherName);
+ console.log(myYoutubeName);
+
+ let user = {
+    upi : "abc@ybl"
+ }
+ let user2 = user;
+ console.log(user2)
+
+ user2.upi = "edr@ert"; // here now user2 and user has changed both because the non-premitive values 
+ // are stored in the Heap and when you access you get reference to that 
+ console.log(user2);
+ console.log(user)
+ 
