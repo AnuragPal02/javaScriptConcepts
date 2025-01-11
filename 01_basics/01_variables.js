3c3a7101280bc431d84ba5a1e22797a496e8fd39
Anurag Pal <anuragpal7754@gmail.com>
2025-01-11 10:02:49 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+
+let accountId = 12345;
+const name = "hey";
+const number = 3;
+const b = "aur"
+var a = 9;
+
+console.log([accountId,name,number,b,a]);
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,10 @@
+let score = 990;
+
+let scoreInString = String(score);
+console.log(scoreInString);
+console.log(typeof scoreInString);
+
+let scoreInBoolean = Boolean(score);
+console.log(scoreInBoolean);
+console.log(typeof scoreInBoolean);
+
